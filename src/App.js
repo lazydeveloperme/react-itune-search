9220fdcf3e65104ce12fc9cc0c0af0c55859409c
lazydeveloperme <lazydeveloper.me@protonmail.com>
2019-12-19 14:20:30 -0500

    feat: add card

--- src/App.js
+++ src/App.js
@@ -9,15 +9,24 @@ import {
   Grid,
   TextField,
   Button,
-  Box
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions
 } from '@material-ui/core';
 import { PermMedia } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   appBarIcon: {
     marginRight: theme.spacing(2)
+  },
+  searchGrid: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3)
   }
 }));
+
+const cards = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
 
 function App() {
   const classes = useStyles();
@@ -32,18 +41,39 @@ function App() {
         </Toolbar>
       </AppBar>
       <Container maxWidth="lg">
-        <Box mt={3}>
-          <Grid container spacing={3} alignItems="center">
-            <Grid item xs={8} sm={9} lg={10}>
-              <TextField label="Search Term" fullWidth />
-            </Grid>
-            <Grid item xs={4} sm={3} lg={2}>
-              <Button variant="contained" color="primary" size="large" fullWidth>
-                Search
-              </Button>
-            </Grid>
+        <Grid container spacing={3} alignItems="center" className={classes.searchGrid}>
+          <Grid item xs={8} sm={9} lg={10}>
+            <TextField label="Search Term" fullWidth />
           </Grid>
-        </Box>
+          <Grid item xs={4} sm={3} lg={2}>
+            <Button variant="contained" color="primary" size="large" fullWidth>
+              Search
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container spacing={4}>
+          {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia image="https://source.unsplash.com/random" title="Image title" />
+                <CardContent>
+                  <Typography component="h2" variant="h5">
+                    Heading
+                  </Typography>
+                  <Typography>This is a media card. You can use this section to describe the content.</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    View
+                  </Button>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </Fragment>
   );
