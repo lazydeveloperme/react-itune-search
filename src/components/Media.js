bae72b0f5ba6b2ce006979e87c306330bed6e01d
lazydeveloperme <lazydeveloper.me@protonmail.com>
2019-12-19 18:20:23 -0500

    fix: adjust width and size of media

--- src/components/Media.js
+++ src/components/Media.js
@@ -11,9 +11,13 @@ function Media({ media }) {
   const classes = useStyles();
 
   return (
-    <Grid item xs={6} sm={3} md={2}>
-      <Card>
-        <CardMedia image={media.artworkUrl100} title="Image title" className={classes.cardMedia} />
+    <Grid item xs={6} sm={4} md={3} lg={2}>
+      <Card style={{ height: '100%' }}>
+        <CardMedia
+          image={media.artworkUrl100.replace('100x100bb', '300x300bb')}
+          title="Image title"
+          className={classes.cardMedia}
+        />
         <CardContent>
           <Typography component="h2" variant="h5">
             {media.artistName}
