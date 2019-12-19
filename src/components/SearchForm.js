583a5da546e4964210e08e43fe247954cd38d6fc
lazydeveloperme <lazydeveloper.me@protonmail.com>
2019-12-19 18:28:12 -0500

    feat: add formik validation

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,6 +3,7 @@ import { useFormik } from 'formik';
 import { TextField, Button, Grid, Box } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { getMedia } from '../store/media';
+import * as Yup from 'yup';
 
 function SearchForm() {
   const dispatch = useDispatch();
@@ -11,6 +12,9 @@ function SearchForm() {
     initialValues: {
       search: ''
     },
+    validationSchema: Yup.object().shape({
+      search: Yup.string().required('Required')
+    }),
     onSubmit: ({ search }) => {
       dispatch(getMedia(search));
     }
@@ -18,9 +22,16 @@ function SearchForm() {
 
   return (
     <Box component="form" onSubmit={formik.handleSubmit} mt={3} mb={3}>
-      <Grid container spacing={3} alignItems="center">
+      <Grid container spacing={3} alignItems="flex-start">
         <Grid item xs={8} sm={9} lg={10}>
-          <TextField name="search" inputProps={formik.getFieldProps('search')} label="Search Term" fullWidth />
+          <TextField
+            error={formik.errors.search && formik.touched.search}
+            helperText={formik.errors.search}
+            name="search"
+            inputProps={formik.getFieldProps('search')}
+            label="Search Term"
+            fullWidth
+          />
         </Grid>
         <Grid item xs={4} sm={3} lg={2}>
           <Button type="submit" variant="contained" color="primary" size="large" fullWidth>
