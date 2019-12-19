d02b8cb3a59fbdb566db587f1acdfe150d34c782
lazydeveloperme <lazydeveloper.me@protonmail.com>
2019-12-19 17:04:36 -0500

    feat: add media reducer

--- src/store/media.js
+++ src/store/media.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import Axios from 'axios';
 
 const media = createSlice({
   name: 'media',
@@ -10,6 +11,29 @@ const media = createSlice({
   reducers: {
     getMediaStart(state) {
       state.isLoading = true;
+    },
+    getMediaSuccess(state, { payload }) {
+      state.media = payload;
+      state.isLoading = false;
+      state.error = null;
+    },
+    getMediaFailed(state, { payload }) {
+      state.isLoading = false;
+      state.error = payload;
     }
   }
 });
+
+export const { getMediaStart, getMediaSuccess, getMediaFailed } = media.actions;
+
+export default media.reducer;
+
+export const getMedia = (search) => async (dispatch) => {
+  try {
+    dispatch(getMediaStart());
+    const { data } = await Axios.get(`https://itunes.apple.com/search?term=${search}`);
+    dispatch(getMediaSuccess(data.results));
+  } catch (error) {
+    dispatch(getMediaFailed(error.toString()));
+  }
+};
