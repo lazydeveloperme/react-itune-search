113e32f1777bba9822148fa6be5e6916ee28f937
lazydeveloperme <lazydeveloper.me@protonmail.com>
2019-12-19 17:48:16 -0500

    feat: redux integration in ITuneSearch

--- src/components/ITuneSearch.js
+++ src/components/ITuneSearch.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Container, Grid } from '@material-ui/core';
+import { Container, Grid, Typography, CircularProgress } from '@material-ui/core';
 import Media from './Media';
 import SearchForm from './SearchForm';
-
-const numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+import { useSelector } from 'react-redux';
 
 function ITuneSearch() {
+  const { media, isLoading, error } = useSelector((state) => state.media);
+
   return (
     <Container maxWidth="lg">
       <SearchForm />
       <Grid container spacing={4}>
-        {numbers.map((number) => <Media key={number} />)}
+        {error ? (
+          <Typography variant="h3" color="error.main">
+            {error}
+          </Typography>
+        ) : isLoading ? (
+          <CircularProgress />
+        ) : (
+          media && media.map((media) => <Media media={media} />)
+        )}
       </Grid>
     </Container>
   );
--- src/components/Media.js
+++ src/components/Media.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { makeStyles, Button, Card, CardMedia, CardContent, CardActions, Grid, Typography } from '@material-ui/core';
-import { useSelector } from 'react-redux';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   cardMedia: {
     paddingTop: '56.25%' // 16:9
   }
 }));
 
-function Media() {
+function Media({ media }) {
   const classes = useStyles();
-  const { media, isLoading, error } = useSelector((state) => state.media);
 
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card>
-        <CardMedia image="https://source.unsplash.com/random" title="Image title" className={classes.cardMedia} />
+        <CardMedia image={media.artworkUrl100} title="Image title" className={classes.cardMedia} />
         <CardContent>
           <Typography component="h2" variant="h5">
-            Heading
+            {media.artistName}
           </Typography>
-          <Typography>This is a media card. You can use this section to describe the content.</Typography>
+          <Typography>
+            {media.collectionName} - {media.trackName}
+          </Typography>
         </CardContent>
         <CardActions>
           <Button size="small" color="primary">
