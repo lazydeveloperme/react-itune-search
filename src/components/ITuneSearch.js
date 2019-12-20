0f6871ab38fee6254f002a85afa934836b4f8eca
lazydeveloperme <lazydeveloper.me@protonmail.com>
2019-12-19 23:02:41 -0500

    refactor: improve ITuneSearch readability

--- src/components/ITuneSearch.js
+++ src/components/ITuneSearch.js
@@ -5,37 +5,31 @@ import SearchForm from './SearchForm';
 import { useSelector } from 'react-redux';
 
 function ITuneSearch() {
-  const { media, isLoading, error } = useSelector((state) => state.media);
+  const { media, isRequested, isLoading, error } = useSelector((state) => state.media);
 
   return (
     <Container maxWidth="lg">
       <SearchForm />
-      {media == null && !isLoading ? (
-        <Box mt={3}>
-          <Typography variant="h5" color="primary" align="center">
-            Enter keyword and click search
-          </Typography>
-        </Box>
-      ) : error ? (
-        <Box mt={3}>
-          <Typography variant="h5" color="secondary" align="center">
-            {error}
-          </Typography>
-        </Box>
-      ) : isLoading ? (
-        <Box mt={3} display="flex" justifyContent="center">
-          <CircularProgress />
-        </Box>
-      ) : media.length === 0 ? (
-        <Box mt={3}>
-          <Typography variant="h5" align="center">
-            No result found
-          </Typography>
-        </Box>
-      ) : (
+      {media.length ? (
         <Grid container spacing={3}>
           {media.map((media) => <Media key={media.trackId} media={media} />)}
         </Grid>
+      ) : (
+        <Box mt={3} display="flex" justifyContent="center">
+          {isLoading ? (
+            <CircularProgress />
+          ) : error ? (
+            <Typography variant="h5" color="secondary">
+              {error}
+            </Typography>
+          ) : isRequested ? (
+            <Typography variant="h5">No result found</Typography>
+          ) : (
+            <Typography variant="h5" color="primary">
+              Enter keyword and click search
+            </Typography>
+          )}
+        </Box>
       )}
     </Container>
   );
--- src/store/media.js
+++ src/store/media.js
@@ -4,33 +4,37 @@ import axios from 'axios';
 const media = createSlice({
   name: 'media',
   initialState: {
+    isRequested: false,
     isLoading: false,
     error: null,
-    media: null
+    media: []
   },
   reducers: {
-    getMediaStart(state) {
+    getMediaRequest(state) {
       state.isLoading = true;
+      state.media = [];
+      state.isRequested = true;
     },
     getMediaSuccess(state, { payload }) {
-      state.media = payload;
       state.isLoading = false;
+      state.media = payload;
       state.error = null;
     },
     getMediaFailed(state, { payload }) {
       state.isLoading = false;
+      state.media = [];
       state.error = payload;
     }
   }
 });
 
-export const { getMediaStart, getMediaSuccess, getMediaFailed } = media.actions;
+export const { getMediaRequest, getMediaSuccess, getMediaFailed } = media.actions;
 
 export default media.reducer;
 
 export const getMedia = (search) => async (dispatch) => {
   try {
-    dispatch(getMediaStart());
+    dispatch(getMediaRequest());
     const { data } = await axios.get(`https://itunes.apple.com/search?term=${search}`);
     dispatch(getMediaSuccess(data.results));
   } catch (error) {
