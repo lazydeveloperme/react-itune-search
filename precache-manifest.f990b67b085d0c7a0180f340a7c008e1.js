4a16207ae221eb29fd6af9b5a1258770110263a8
lazydeveloperme <lazydeveloper.me@protonmail.com>
2019-12-26 14:55:39 -0500

    Updates

---
+++ precache-manifest.f990b67b085d0c7a0180f340a7c008e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cce52e482a079695d3a3abb78ed4461",
+    "url": "/react-test-itune-search/index.html"
+  },
+  {
+    "revision": "8003c886df54686349e7",
+    "url": "/react-test-itune-search/static/js/2.0d460498.chunk.js"
+  },
+  {
+    "revision": "2fc788dcf2ac9c7fea5d816606999b2d",
+    "url": "/react-test-itune-search/static/js/2.0d460498.chunk.js.LICENSE"
+  },
+  {
+    "revision": "259707e0557b74889ce3",
+    "url": "/react-test-itune-search/static/js/main.5443ce52.chunk.js"
+  },
+  {
+    "revision": "814070cb5c60bd73a48e",
+    "url": "/react-test-itune-search/static/js/runtime-main.e85dc7d3.js"
+  }
+]);
